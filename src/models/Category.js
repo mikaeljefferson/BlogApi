b14e588c02bd598194117a37cb07c54c990816d2
mikaeljefferson <mikaeljeffeson@gmail.com>
2023-06-13 09:39:35 -0300

    Feat: Req 07

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-lines-per-function */
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define(
+'Category',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER,
+        },
+        name: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        timestamps: false,
+        tableName: 'categories',
+        underscored: true,
+      },
+    );
+  
+    return Category;
+  };
--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
+        allowNull: false,
       },
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
